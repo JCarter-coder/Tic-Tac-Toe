9b2beaeec27d3945ef7bb5d3ae41f5293a516dc2
Jesse_Carter <jessetcarter@hotmail.com>
2023-12-12 13:27:59 -0700

    "Updated css"

--- tic-tac-toe-app/src/App.jsx
+++ tic-tac-toe-app/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+
+
+function Square() {
+  return (
+    <button className="square">X</button>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+
   return (
-    <>
+    <Square />
+    /*<>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +38,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </>*/
   )
 }
 
